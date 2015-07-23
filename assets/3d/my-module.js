2865ab9f4bac84095544ebf6d1326e1a0941ed3d
DavidDai <dai_yunbo@foxmail.com>
2015-07-23 01:48:05 +0000

    Add module my-module

---
+++ assets/3d/my-module.js
@@ -0,0 +1,15 @@
+/**
+ * @module assets/3d/my-module
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class MyModule
+ * @extends Montage
+ */
+exports.MyModule = Montage.specialize(/** @lends MyModule# */ {
+    constructor: {
+        value: function MyModule() {
+            this.super();
+        }
+    }
+});

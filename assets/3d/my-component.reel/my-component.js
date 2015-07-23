7df794cbe888c6e365a8d16eb7fe416591a02c2c
DavidDai <dai_yunbo@foxmail.com>
2015-07-23 01:47:49 +0000

    Add component my-component

---
+++ assets/3d/my-component.reel/my-component.js
@@ -0,0 +1,16 @@
+/**
+ * @module assets/3d/my-component.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class MyComponent
+ * @extends Component
+ */
+exports.MyComponent = Component.specialize(/** @lends MyComponent# */ {
+    constructor: {
+        value: function MyComponent() {
+            this.super();
+        }
+    }
+});
